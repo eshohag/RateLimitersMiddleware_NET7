dd108036104183ba29f6889ca9d5b8b2cd18703f
MD Shohag Mia <eshohag@outlook.com>
2023-02-11 19:40:44 +0600

    Add project files.

---
+++ RateLimitersMiddleware_NET7/Controllers/FixedWindowFCController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace RateLimitersMiddleware_NET7.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [EnableRateLimiting("FixedWindow")]
+    public class FixedWindowFCController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<FixedWindowFCController> _logger;
+
+        public FixedWindowFCController(ILogger<FixedWindowFCController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ RateLimitersMiddleware_NET7/Program.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace RateLimitersMiddleware_NET7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Throttle the thread pool (set available threads to amount of processors)
+            //ThreadPool.SetMaxThreads(Environment.ProcessorCount, Environment.ProcessorCount);
+            builder.Services.AddControllers();
+            builder.Services.AddRateLimiter(options =>
+            {
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+                    RateLimitPartition.GetFixedWindowLimiter(
+                        partitionKey: httpContext.User.Identity?.Name ?? httpContext.Request.Headers.Host.ToString(),
+                        factory: partition => new FixedWindowRateLimiterOptions
+                        {
+                            AutoReplenishment = true,
+                            PermitLimit = 10,
+                            QueueLimit = 5,
+                            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                            Window = TimeSpan.FromSeconds(1)
+                        }));
+
+                options.AddFixedWindowLimiter("FixedWindow", options =>
+                {
+                    options.AutoReplenishment = true;
+                    options.PermitLimit = 10;
+                    options.QueueLimit = 5;
+                    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    options.Window = TimeSpan.FromSeconds(1);
+                });
+                options.AddSlidingWindowLimiter("SlidingWindow", options =>
+                {
+                    options.AutoReplenishment = true;
+                    options.PermitLimit = 10;
+                    options.QueueLimit = 5;
+                    options.SegmentsPerWindow = 1;
+                    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    options.Window = TimeSpan.FromSeconds(1);
+                });
+                options.AddConcurrencyLimiter("Concurrency", options =>
+                {
+                    options.PermitLimit = 2;
+                    options.QueueLimit = 1;
+                    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                });
+                options.OnRejected = async (context, token) =>
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                    if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    {
+                        await context.HttpContext.Response.WriteAsync(
+                            $"Too many requests. Please try again after {retryAfter.TotalMinutes} minute(s). " +
+                            $"Read more about our rate limits at https://example.org/docs/ratelimiting.", cancellationToken: token);
+                    }
+                    else
+                    {
+                        await context.HttpContext.Response.WriteAsync(
+                            "Too many requests. Please try again later. " +
+                            "Read more about our rate limits at https://example.org/docs/ratelimiting.", cancellationToken: token);
+                    }
+                };
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.UseRateLimiter();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
