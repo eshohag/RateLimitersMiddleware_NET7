d98d501ffdfe39262c023f7a88138df12e0c72f8
MD Shohag Mia <eshohag@outlook.com>
2023-02-11 20:38:33 +0600

    WeatherForecast

--- RateLimitersMiddleware_NET7/Controllers/WeatherForecastController.cs
+++ RateLimitersMiddleware_NET7/Controllers/WeatherForecastController.cs
@@ -10,8 +10,8 @@ namespace RateLimitersMiddleware_NET7.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
